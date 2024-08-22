b3a42698aa0fb1a7e946377c49ea91913aa370b0
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-08-22 17:35:51 -0300

    Add files via upload

---
+++ vetor/vetor/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+int[] numeros = new int[5];
+int r = 0, soma = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite um numero:");
+    r = Convert.ToInt32(Console.ReadLine());
+    numeros[i] = r;
+    if ((i % 2) == 0)
+    {
+        soma = soma + numeros[i];
+        Console.WriteLine(numeros[i]);
+    }
+};
+Console.WriteLine(soma);
