365a37ba9fa6f99c3c9874a75dd72ffe3bd618bf
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-08-29 16:52:54 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/bin/Debug/net8.0/Atividade1Exercicio1/Atividade1Exercicio1/Program.cs
@@ -0,0 +1,61 @@
+﻿//for (int i = 0; i < 50; i++)
+//{
+//    Console.WriteLine(i+1);
+//};
+
+////-----------------------------
+
+//int calc, num, tem = 0;
+
+//Console.WriteLine("Digite um numero");
+//num = Convert.ToInt32(Console.ReadLine());
+//for (int i = 1; i < num + 1 ; i++) {
+//    calc = num + i;
+//    tem = tem + i;
+//};
+//Console.WriteLine("A soma de todos os numeros, até o numero "+num+" é = "+tem);
+
+//////-----------------------------
+
+//int num, numMaior = 0;
+
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine("Digite o " + (i+1) + "º numero");
+//    num = Convert.ToInt32(Console.ReadLine());
+//    if (num > numMaior) {
+//        numMaior = num;
+//    };
+//};
+//Console.WriteLine("O maior numero da lista é: "+numMaior);
+
+//////-----------------------------
+
+//int c = 0, num = 1, numMaior = 0;
+
+//while (num != 0) {
+//    c++;
+//    Console.WriteLine("Digite o "+c+"º Numero");
+//    num = Convert.ToInt32(Console.ReadLine());
+//    if (num > numMaior){
+//        numMaior = num;
+//    };
+//};
+//Console.WriteLine("O maior numero digitado na sequencia foi: "+numMaior);
+
+//////-----------------------------
+
+//for (int i = 0; i < 1000; i = i + 10) {
+//    Console.WriteLine(i+10);
+//};
+
+//////-----------------------------
+
+////int num, fatorial = 1;
+
+////Console.WriteLine("Digite um numero");
+////num = int.Parse(Console.ReadLine());
+////for (int i = 2; i <= num ; i++) {
+////    fatorial *= i;
+////};
+////Console.WriteLine("O fatorial desse numero é: "+fatorial);
