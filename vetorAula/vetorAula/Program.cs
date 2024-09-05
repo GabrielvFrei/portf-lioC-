c19d444fbace31bc730388561f4170ba9fb3ce21
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-09-05 15:22:28 -0300

    Add files via upload

---
+++ vetorAula/vetorAula/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*int[] numeros = new int[5];
+int soma = 0;
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Digite um numero");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+    soma = numeros[i] + soma;
+}
+Console.WriteLine("A soma dos números digitados é "+soma); */
+
+
+/*int[] numeros = new int[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Digite um numero");
+    numeros[i] = Convert.ToInt32(Console.ReadLine());
+};
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (numeros[i] % 2 != 0){
+        Console.WriteLine("O número "+numeros[i]+" é impar");
+    }
+};*/
+
+/*Double[] numeros = new double[10];
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine("Digite um numero");
+    numeros[i] = Convert.ToDouble(Console.ReadLine());
+};
+for (int i = 0; i < numeros.Length; i++) {
+    if (numeros[i] % 2 != 0)
+    {
+        numeros[i] = (numeros[i] * 0.03 + numeros[i]);
+    }
+    else
+    {
+        numeros[i] = (numeros[i] * 0.04 + numeros[i]);
+    }
+    Console.WriteLine(numeros[i]);
+};*/
+
+
+
