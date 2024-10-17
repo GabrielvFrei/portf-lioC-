026cb6aea91aa4081717a0e792b75b85fdc2dfa1
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-10-17 14:31:34 -0300

    Add files via upload

---
+++ aula10/aula10/Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula10
+{
+    internal class Pessoa
+    {
+        private string nome;
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public Pessoa()
+        {
+            this.nome = "invalido";
+
+        }
+    }
+
+}
+
