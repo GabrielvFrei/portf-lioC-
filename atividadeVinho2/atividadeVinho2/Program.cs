0ef0fa912e49cac2232bab71bdae90d0c9c3b863
GabrielAlvesFR <137641904+GabrielvFrei@users.noreply.github.com>
2024-10-20 12:19:03 -0300

    Add files via upload

---
+++ atividadeVinho2/atividadeVinho2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using atividadeVinho;
+
+Vinho v = new Vinho();
+int opc;
+do {
+    Console.WriteLine("Menu: \r\n" +
+        "1 - Cadastrar Vinho\r\n" +
+        "2 - Verificar Preço\r\n" +
+        "3 - Mostrar Vinhor\r\n" +
+        "4 - Sair");
+    opc = Convert.ToInt16(Console.ReadLine());
+    switch (opc)
+    {
+        case 1:
+            Console.WriteLine("digite um nome");
+            string nome = Console.ReadLine();
+            v.setNome(nome);
+            Console.WriteLine("digite um tipo");
+            string tipo = Console.ReadLine();
+            v.setTipo(tipo);
+            Console.WriteLine("digite um preco");
+            double preco = Convert.ToInt16(Console.ReadLine());
+            v.setPreco(preco);
+            Console.WriteLine("digite uma safra");
+            int safra = Convert.ToInt16(Console.ReadLine());
+            v.setSafra(safra);
+            break;
+
+        case 2:
+            if (v.verificaPreco() == true)
+            {
+                Console.WriteLine("Produto em oferta!");
+            }
+            else
+            {
+                Console.WriteLine("Produto com preço normal");
+            }
+            break;
+
+        case 3:
+            Console.WriteLine(v.mostrarVinho());
+            break;
+
+        case 4:
+            Console.WriteLine("");
+            break;
+        default:
+            Console.WriteLine("opção invailda");
+            break;
+    };
+}while (opc != 4);
+
---
+++ atividadeVinho2/atividadeVinho2/Vinho.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividadeVinho
+{
+    internal class Vinho
+    {
+        private String nome;
+        private String tipo;
+        private double preco;
+        private int safra;
+
+        public string mostrarVinho()
+        {
+            return $"Nome do vinho: {nome} \r\n tipo do vinho: {tipo} \r\n Preço do vinho: {preco}\r\n Safra do Vinho: {safra}";
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome() {
+            return this.nome;
+        }
+        public void setTipo(string tipo)
+        {
+            this.tipo = tipo;
+        }
+        public string getTipo()
+        {
+            return this.tipo;
+        }
+        public void setPreco(double preco)
+        {
+            this.preco = preco;
+        }
+        public double getPreco()
+        {
+            return this.preco;
+        }
+        public void setSafra(int safra)
+        {
+            this.safra = safra;
+        }
+        public int getSafra()
+        {
+            return this.safra;
+        }
+
+        public Boolean verificaPreco() {
+            if (preco < 25)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
